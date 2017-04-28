bff0332837e43d8ad7cb9b8f27a7f13cb0f3262e
YagiGo <jeremywu1995@gmail.com>
2017-04-28 11:18:15 +0800

    Add Sorting Function

--- core/DataAnalysis.py
+++ core/DataAnalysis.py
@@ -50,20 +50,26 @@ print(numberOfReviewPerDate)
 '''
 
 def dataAnalysis(inPath, outPath):
+    countOfReviewWithNoContent = 0
+    countOfReviewWithContent = 0
     reviewUser = []
     reviewContent = []
     reviewDateAndTime = []
     reviewDate = []
     dateWithReviews = []
     numberOfReviewsOnSameDate = []
+    noEmotionReview = []
     start = 0
     numberOfReviews = 0
-    item = 0
     df = pd.read_csv(inPath, error_bad_lines = False, header = None)
     # df_names = pd.read_csv('C:\workspace\SentimentClassfication\input.csv',
     #                       error_bad_lines = False, header = None, names = ['name', 'date', 'content'])
-    rowCount = len(df.index)  # 获取总行数
-    for row in range(rowCount):
+    # 获取总行数
+    df = df.sort_values(by = [1], ascending = False)
+    #print(df)
+    #df.to_csv('C:\workspace\SentimentClassfication\SortTest.csv', index=False, header=False)
+
+    for row in range(len(df.index)):
         reviewUser.append(df.ix[row][0])
         reviewDateAndTime.append(df.ix[row][1])
         reviewContent.append(df.ix[row][2])
@@ -98,9 +104,10 @@ def dataAnalysis(inPath, outPath):
     df_date.to_csv(outPath, index = False, header = False)
     wordCut = thulac.thulac()
     #加入进度条显示功能，之后可能会删去
-    '''
+
     p = progressbar.ProgressBar(filt = True)
     p.start(len(reviewContent))
+    countOfReview = len(reviewContent)
     for item in range(len(reviewContent)):
         try:
             reviewContent[item] = wordCut.cut(reviewContent[item], text = True)
@@ -109,11 +116,14 @@ def dataAnalysis(inPath, outPath):
             p.update(item + 1)
             continue
     p.finish()
+
+
     #for item in range(len(reviewContent)):
     #    print(reviewContent[item])
     #pattern1 = re.compile(r'[\s]*?[\S]*_w') #除去标点
     #pattern2 = re.compile(r'[[\S]*_n [\S]*_d [\S]*_v]?') #名词+副词+动词 如: 质量 还 行
     #pattern3 = re.compile(r'(\S*_n)+\s?(\S*_d)?\s?(\S*_v)?\s?(\S*_a)+') #名词+形容词 如：外观漂亮
+
     pattern = re.compile(r'(\S*_n)?\s?(\S*_d)?\s?(\S*_v)?\s?(\S*_a)+') #名词(可能出现）+
                                                                         # 副词（可能出现）+
                                                                         #动词（可能出现）+形容词（一定出现）
@@ -131,14 +141,26 @@ def dataAnalysis(inPath, outPath):
         #    emotionWords.append(nounAdjWords[i])
         for i in range(len(emotionWords)):
             emotionWords[i] =  ''.join(emotionWords[i])
+        if len(emotionWords) == 0:
+            noEmotionReview.append(reviewContent[item])
         reviewContent[item] = emotionWords
+        if len(reviewContent[item]) == 0:
+            countOfReviewWithNoContent += 1
+        else:
+            countOfReviewWithContent += 1
         p.update(item + 1)
     p.finish()
     print(reviewContent)
-'''
+    print("无内容评论数：{}".format(countOfReviewWithNoContent))
+    print("有内容评论数：{}".format(countOfReviewWithContent))
+    print("内容分析失败评论数量：{}".format(countOfReview - countOfReviewWithContent - countOfReviewWithNoContent))
+    print("正面评价数：开发中...")
+    print("负面评价数：开发中...")
+    print(noEmotionReview)
 
 reviewUser = []
 reviewContent = []
+
 inPath = 'C:\workspace\SentimentClassfication\largeDataTest.csv'
 outPath = 'C:\workspace\SentimentClassfication\ReviewNumberPerDate.csv'
 dataAnalysis(inPath, outPath)
